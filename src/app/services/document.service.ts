6a4e7456276e0cc8c29e6b4a9e8ba06b5b89f1ee
harshgoel05 <harshgoel05@gmail.com>
2020-01-18 16:16:07 +0530

    email verification

--- src/app/services/document.service.ts
+++ src/app/services/document.service.ts
@@ -1,9 +1,9 @@
-import { Injectable } from '@angular/core';
+import { Injectable , NgZone } from '@angular/core';
 import { Document } from '../_model/document';
 import { AngularFirestore } from "@angular/fire/firestore";
 import { AngularFireAuth } from "@angular/fire/auth";
 import { Observable } from 'rxjs';
- 
+import { Router } from "@angular/router";
 @Injectable({
 providedIn: 'root'
 })
@@ -11,29 +11,35 @@ providedIn: 'root'
 
 export class DocumentService {
     userData: Observable<firebase.User>;
-  constructor(private firestore: AngularFirestore,private angularFireAuth: AngularFireAuth) {
+  constructor(private firestore: AngularFirestore,private angularFireAuth: AngularFireAuth ,  public router: Router, public ngZone: NgZone) {
     this.userData = angularFireAuth.authState;
     }
      
-
-  /* Add Document */
-  AddDocument(document: Document) {
-    this.angularFireAuth
-    .auth
-    .createUserWithEmailAndPassword(document.uemail, document.upassword)
-    .then(res => {
-    console.log('You are Successfully signed up!', res);
-    })
-    .catch(error => {
-    console.log('Something is wrong:', error.message);
-    });
-    return new Promise<any>((resolve, reject) => {
-      this.firestore.collection("documents").add(document)
+    SendVerificationMail() {
+        return this.angularFireAuth.auth.currentUser.sendEmailVerification()
+        .then(() => {
+          this.router.navigate(['adminpanel']);
+        })
+      }
+/* Add Document */
+    AddDocument(document: Document) {
+        this.angularFireAuth.auth.createUserWithEmailAndPassword(document.uemail, document.upassword)
         .then(res => {
-          resolve(res);
-        }, err => reject(err));
-    });
-        }
+            this.SendVerificationMail();
+            console.log('You are Successfully signed up!', res);
+        })
+            .catch(error => {
+            console.log('Something is wrong:', error.message);
+        });
+
+
+        return new Promise<any>((resolve, reject) => {
+            this.firestore.collection("documents").add(document)
+            .then(res => {
+                resolve(res);
+            }, err => reject(err));
+        });
+    }
 
   // /* Get Document */
   GetDocument(id: string) {
@@ -57,10 +63,12 @@ export class DocumentService {
   }
     //   Signin
   SignIn(email: string, password: string) {
-    this.angularFireAuth
-    .auth
-    .signInWithEmailAndPassword(email, password)
+    this.angularFireAuth.auth.signInWithEmailAndPassword(email, password)
     .then(res => {
+        if (res.user.emailVerified !== true) {
+            this.SendVerificationMail();
+            window.alert('Please validate your email address. Kindly check your inbox.');
+          }
     console.log('You are Successfully logged in!');
     })
     .catch(err => {
