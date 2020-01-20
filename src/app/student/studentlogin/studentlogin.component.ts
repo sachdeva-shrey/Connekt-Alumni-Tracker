dfa3a07a9274471e6870118375ef689c3fc14a5e
Shrey Sachdeva <shreysachdeva.2000@gmail.com>
2020-01-21 02:49:48 +0530

    student log in

--- src/app/student/studentlogin/studentlogin.component.ts
+++ src/app/student/studentlogin/studentlogin.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-studentlogin',
-  templateUrl: './studentlogin.component.html',
-  styleUrls: ['./studentlogin.component.css']
+  selector: 'app-login',
+  templateUrl: '../studentlogin/studentlogin.component.html',
+  styleUrls: ['../studentlogin/studentlogin.component.css']
 })
-export class StudentloginComponent implements OnInit {
 
-  constructor() { }
+export class StudentloginComponent {
+    constructor(private router : Router) { }
 
-  ngOnInit() {
+  verify(studentloginform){
+    this.router.navigate(["adminpanel"]);
   }
-
-}
+  }
+  
