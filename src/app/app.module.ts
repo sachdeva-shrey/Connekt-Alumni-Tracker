ba58e3d8792d8e632947725f15418173ba15a57b
Yakshchopra <yakshchopra@gmail.com>
2020-01-20 23:01:17 +0530

    added chart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ChartsModule } from 'ng2-charts';
 /*Firebase */
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from "@angular/fire/firestore";
@@ -71,7 +71,8 @@ import { AdminchartsComponent } from './adminpanel/admincharts/admincharts.compo
     FormsModule,
     ReactiveFormsModule,
     AngularFireModule.initializeApp(environment.firebaseConfig,'AlumniReg'),
-    AngularFirestoreModule
+    AngularFirestoreModule,
+    ChartsModule
   ],
   providers: [DocumentService,AngularFireAuth],
   bootstrap: [AppComponent]
